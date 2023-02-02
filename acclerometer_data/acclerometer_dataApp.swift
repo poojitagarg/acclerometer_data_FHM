f0757aafd650bc3258a64081c033132bd79ccc6b
Poojita Garg <poojita8garg@gmail.com>
2023-02-02 14:52:18 -0500

    Initial Commit

---
+++ acclerometer_data/acclerometer_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  acclerometer_dataApp.swift
+//  acclerometer_data
+//
+//  Created by School on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct acclerometer_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
